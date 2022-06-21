1f923813e039ac3fe915f02166bdaa3e74662738
Wiktor Stepniewski <wiktor.stepniewski1@gmail.com>
2022-06-21 02:34:05 +0200

    lab13

--- lab11/src/main/scala/agh/wggios/analizadanych/Main.scala
+++ lab11/src/main/scala/agh/wggios/analizadanych/Main.scala
@@ -3,14 +3,22 @@ import agh.wggios.analizadanych.datareader.DataReader
 import agh.wggios.analizadanych.caseclass.FlightCaseClass
 import agh.wggios.analizadanych.transformations.Transformations
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.Logger
 
 
 object Main {
+  val logger: Logger = Logger.getRootLogger
+  val spark: SparkSession = SparkSession.builder().config("spark.master", "local").getOrCreate()
+  import spark.implicits._
 
   def main(args: Array[String]): Unit = {
-    val spark: SparkSession = SparkSession.builder().config("spark.master", "local").getOrCreate()
-    import spark.implicits._
+    logger.info("----------------------------------------------------------------------------------")
+    logger.info("Test log.")
+    //val spark: SparkSession = SparkSession.builder().config("spark.master", "local").getOrCreate()
+    //import spark.implicits._
     val df = new DataReader().read_csv("2010-summary.csv").as[FlightCaseClass]
     df.filter(row => new Transformations().USA_flights(row)).show()
+    logger.info("Everything done correctly.")
+
   }
 }
